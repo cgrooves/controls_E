fb58dd05dce160863655cb5255e6d01eec981568
cgrooves <caleb.j.groves@gmail.com>
2017-09-15 22:04:22 -0600

    Started framework for sim and animation, gui in hw_a

---
+++ ball_beam_params.m
@@ -0,0 +1,12 @@
+% Ball Sim Parameters
+
+R = 2; % ball radius
+L = 10; % length of beam
+
+theta_init = pi/2;
+theta_max = 2*pi;
+theta_min = 0;
+
+z_init = L/2;
+z_min = 0;
+z_max = L;
---
+++ hw_a/ball_paramsHWA.m
@@ -0,0 +1,3 @@
+% load ball on beam parameters
+addpath ./.. % add parent dir
+beam_on_ball_params
